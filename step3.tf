7afddbec1a79743e54dc6bb3c9478fae2d9ba2f7
Madina <madina.shumbolova@gmail.com>
2022-07-27 15:41:12 -0500

    Update step3.tf

--- step3.tf
+++ step3.tf
@@ -9,16 +9,32 @@ resource "aws_db_instance" "mydb" {
    }
 
    resource "aws_route53_record" "database" {
-      zone_id = "${aws_route53_zone.primary.zone_id}"
+      zone_id = "${aws_route53_zone.primary.us-east-1}"
       name = "database.example.com"
       type = "CNAME"
       ttl = "300"
       records = ["${aws_db_instance.default.endpoint}"]
    }
 
-excluded_members = [
+
+resource "aws_rds_cluster_endpoint" "endpoint_type" {
+  cluster_identifier          = aws_rds_cluster.default.id
+  cluster_endpoint_identifier = "reader"
+  custom_endpoint_type        = "READER"
+
+  excluded_members = [
     aws_rds_cluster_instance.reader1.id,
     aws_rds_cluster_instance.reader2.id,
     aws_rds_cluster_instance.reader3.id,
+  ]
+}
+
+resource "aws_rds_cluster_endpoint" "static" {
+  cluster_identifier          = aws_rds_cluster.default.id
+  cluster_endpoint_identifier = "writer"
+  custom_endpoint_type        = "WRITER"
+
+  static_members = [
     aws_rds_cluster_instance.writer.id,
   ]
+}
