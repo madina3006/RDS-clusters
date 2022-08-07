ce397ffe5556a6323c29086a4e647cbc5810a591
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-08-06 23:37:56 -0500

    Delete route53.tf

--- route53.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "project" {
-  zone_id = "Z08986893U1AWC562QW2Q"
-  name    = "writer.kudratillo.org"
-  type    = "CNAME"
-  ttl     = "300"
-  records = ["127.10.0.1"]
-}
